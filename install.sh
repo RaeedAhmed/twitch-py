2ee0a11f2b963f7219caa0d681525256a5f34da8
RaeedAhmed <ruahmed7868@gmail.com>
2021-05-05 06:38:20 -0700

    Complete rewrite with db backup

--- install.sh
+++ install.sh
@@ -1,69 +1,102 @@
 #!/bin/bash
+TEMPDIR=twitch-tmp
 CONFDIR=$HOME/.config/twitch-py
 EXEC=/usr/local/bin/twitch-py
 NAME=twitch-py
 BIN=https://github.com/RaeedAhmed/$NAME/releases/latest/download/$NAME
 REPO=https://github.com/RaeedAhmed/$NAME.git
+BACKUP=/var/local/$NAME
 
-rmConf() {
-if [ -d "$CONFDIR" ]; then
-    echo "Removing config files..."
-    rm -r $CONFDIR
-else
-    echo "No config files found..."
-fi
+setup() {
+    if ! command -v unzip &> /dev/null
+    then
+        echo " > 'unzip' is not installed. Installation Cancelled."
+        exit
+    fi
+    echo " > Starting twitch-py installation..."
+    mkdir $TEMPDIR
+    cd $TEMPDIR
+    if ! [ -d $BACKUP ]; then
+        sudo mkdir -p $BACKUP
+        sudo touch $BACKUP/data.db
+    fi
 }
 
-rmExec() {
-if [ -f "$EXEC" ]; then
-    echo "Removing past installation..."
-    sudo rm $EXEC
-else
-    echo "No past installation..."
-fi
+backupDB() {
+    if [ -f $CONFDIR/data.db ]; then
+        echo " > Backing up database to $BACKUP"
+        sudo mv $BACKUP/data.db $BACKUP/data.db.old
+        sudo mv $CONFDIR/data.db $BACKUP
+    fi
 }
 
-setupConf() {
-rmConf
-echo "Setting up static files..."
-mkdir $CONFDIR
-cp -R twitch_py/config $CONFDIR
-cp -R twitch_py/views $CONFDIR
+rmFiles() {
+    if [ -d "$CONFDIR" ]; then
+        echo " > Removing config files"
+        rm -r $CONFDIR
+    else
+        echo " > No config files found"
+    fi
+    if [ -f "$EXEC" ]; then
+        echo " > Removing past exec"
+        sudo rm $EXEC
+    else
+        echo " > No past exec"
+    fi   
 }
 
-uninstall() {
-rmConf
-rmExec
+dlFiles() {
+    echo " > Downloading executable"
+    sudo curl -sL $BIN | sudo tee $EXEC >/dev/null
+    sudo chmod +x $EXEC
+    echo " > Downloading repository"
+    curl -sL https://github.com/RaeedAhmed/$NAME/archive/refs/heads/master.zip -o $NAME.zip
+    unzip $NAME.zip -d $NAME
+    echo " > Configuring static files"
+    mkdir $CONFDIR
+    cp -R $NAME/src/config $CONFDIR
+    cp -R $NAME/src/views $CONFDIR
+    echo " > Installing Streamlink"
+    pip3 install -q --user streamlink
 }
 
-dlBin() {
-echo "Downloading binary"
-sudo wget -q --show-progress -O $EXEC $BIN || sudo curl -sL $BIN | sudo tee $EXEC >/dev/null
-sudo chmod +x $EXEC
+restoreDB() {
+    if [ -f $BACKUP/data.db.old]; then
+        cp $BACKUP/data.db $CONFDIR
+    fi
 }
 
-rmRepo() {
+clean_() {
     cd ..
-    sudo rm -r $NAME
+    sudo rm -r $TEMPDIR
+    echo " > Installation complete"
 }
 
-while getopts cpu option
+install_() {
+    setup
+    backupDB
+    rmFiles
+    dlFiles
+    restoreDB
+    clean_
+}
+
+uninstall_() {
+    rmFiles
+    if [ -d $BACKUP ]; then
+        sudo rm -r $BACKUP
+    else
+        echo " > No backup database found"
+    fi
+}
+
+while getopts iu option
 do
     case "${option}"
     in
-    u) uninstall; rmRepo; exit 1
+    i) install_; exit 1
+    ;;
+    u) uninstall_; exit 1
     ;;
     esac
 done
-
-# No flags
-git clone $REPO
-cd $NAME
-setupConf
-rmExec
-dlBin
-pip3 install --user --upgrade streamlink
-rmRepo
-
-
-
